feb489d7a4adaf386f81d7cf340d605bf213e15d
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-11-19 22:09:43 +0000

    MVP completed

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> stock;
+    private int capacity;
+
+    public Library(int capacity) {
+        this.stock = new ArrayList<Book>();
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int countStock() {
+        return stock.size();
+    }
+
+    public void addBookToStock(Book newBook) {
+        stock.add(newBook);
+    }
+
+    public Boolean checkIfStockAtCapacity() {
+        return stock.size() >= capacity;
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,48 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    private Library stuffy;
+    private Book dusty;
+
+    @Before
+    public void before() {
+        stuffy = new Library(3);
+        dusty = new Book("Shardik", "Richard Adams", "Fantasy");
+    }
+
+    @Test
+    public void canGetCapacity() {
+        assertEquals(3, stuffy.getCapacity());
+    }
+
+    @Test
+    public void canCountStock() {
+        assertEquals(0, stuffy.countStock());
+    }
+
+    @Test
+    public void canAddBookToStock() {
+        stuffy.addBookToStock(dusty);
+        assertEquals(1, stuffy.countStock());
+
+    }
+
+    @Test
+    public void canTellWhenStockIsAtCapacity() {
+        stuffy.addBookToStock(dusty);
+        stuffy.addBookToStock(dusty);
+        stuffy.addBookToStock(dusty);
+        assertEquals(true, stuffy.checkIfStockAtCapacity());
+    }
+
+    @Test
+    public void canTellWhenStockIsNotAtCapacity() {
+        stuffy.addBookToStock(dusty);
+        stuffy.addBookToStock(dusty);
+        assertEquals(false, stuffy.checkIfStockAtCapacity());
+    }
 }
